3e9b7ff4f0a413b0004fe400f579285103e717ca
Darshu <darshants.darshan1@gmail.com>
2017-08-25 21:20:30 -0700

    hello world learn the basics

--- App.js
+++ App.js
@@ -1,54 +1,13 @@
-import React from 'react';
-import { ActivityIndicator, StyleSheet, Text, View, ListView,Image } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, Text } from 'react-native';
 
-export default class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={
-      isLoading:true
-    }
-  }
-  componentDidMount(){
-    return fetch('https://facebook.github.io/react-native/movies.json')
-          .then ((response)=> response.json())
-          .then((responseJson) => {
-            let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2 });
-            this.setState({
-              isLoading:false,
-              dataSource : ds.cloneWithRows(responseJson.movies)          
-            });
-          })
-          .catch((error) => {
-            console.error(error)
-          });
-  };
-
+export default class HelloWorldApp extends Component {
   render() {
-    let pic = {uri: 'http://www.jasonchristopher.com/wp-content/uploads/2015/07/sexy-girl-bikini.jpg'
-  };
-    if(this.state.isLoading){
     return (
-      <View style={{flex:1, paddingTop: 20}}>
-       <ActivityIndicator/>
-      </View>
+      <Text>Hello world!</Text>
     );
-  }
-   return(
-     <View style={{flex:1, paddingTop:20}}>
-          <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-      
-       <Text> Hello Auto Connector! </Text>  
-       <Text> Chandan </Text>
-       <Text> Hello Chandan </Text>
-
-       <Image source={pic} style={{width: 500, height: 500}}/>
-       <ListView
-       dataSource={this.state.dataSource}
-        renderRow={(rowData) => <Text>{rowData.title}, {rowData.releaseYear}</Text>}
-       />
-     </View>
-   );
-
   }
 }
 
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => HelloWorldApp);
