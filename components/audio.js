4b82e480b718f359abe9d43fb649b1dbf7cc0f29
Darshu <darshants.darshan1@gmail.com>
2017-09-03 20:15:24 -0700

    state

--- components/audio.js
+++ components/audio.js
@@ -1,10 +1,23 @@
 import React from 'react'
-import {Text} from 'react-native'
+import {Text, View} from 'react-native'
+class Greetings extends React.Component {
+    render(){
+        return (
+            <Text> Hello {this.props.name}! </Text>
+           
+        );
+    }
+}
 
  export default class Audio extends React.Component{
 
     render(){
-        return(<Text> Hello from Audio </Text>)
+        return(
+            <View> 
+                <Greetings name='Tame'/>
+                <Greetings name='newTame'/>
+            </View>
+        )
     }
 
 }
